ba6eff7b66c1ab55e24f31f3920e72f1928b6437
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-25 18:45:41 +0900

    Implement edit,update,destroy to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def new
     @user = User.new
   end
@@ -6,18 +7,37 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect to user_path(@user.id)
+      redirect_to user_path(@user.id)
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "アカウントを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to new_user_path, notice: "アカウントを削除しました"
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
