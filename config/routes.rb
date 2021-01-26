2f79414826f7eeacc7473478c5816d17eb56f38e
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-26 11:25:42 +0900

    Create comment model and table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :blogs do
+    resources :comments
+  end
 end
